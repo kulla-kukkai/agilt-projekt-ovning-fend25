36431b35b999c43f81253cc9a7b8fdff35e538a8
BassamR14 <bassam.ruwaida@gmail.com>
2026-03-13 11:04:39 +0100

    bugfix for delete button not removing player

--- app.js
+++ app.js
@@ -1,47 +1,41 @@
-let teamA = JSON.parse(localStorage.getItem("teamA")) || []
-let teamB = JSON.parse(localStorage.getItem("teamB")) || []
+let teamA = JSON.parse(localStorage.getItem("teamA")) || [];
+let teamB = JSON.parse(localStorage.getItem("teamB")) || [];
 
-let teamAName = localStorage.getItem("teamAName") || "Team A"
-let teamBName = localStorage.getItem("teamBName") || "Team B"
-
+let teamAName = localStorage.getItem("teamAName") || "Team A";
+let teamBName = localStorage.getItem("teamBName") || "Team B";
 
 function save() {
+  localStorage.setItem("teamA", JSON.stringify(teamA));
+  localStorage.setItem("teamB", JSON.stringify(teamB));
 
-    localStorage.setItem("teamA", JSON.stringify(teamA))
-    localStorage.setItem("teamB", JSON.stringify(teamB))
-
-    localStorage.setItem("teamAName", teamAName)
-    localStorage.setItem("teamBName", teamBName)
-
+  localStorage.setItem("teamAName", teamAName);
+  localStorage.setItem("teamBName", teamBName);
 }
-
 
 function renameTeam(team) {
-
-    if (team === "A") {
-        const val = document.getElementById("teamAInput").value
-        if (val) teamAName = val
-    }
-    if (team === "B") {
-        const val = document.getElementById("teamBInput").value
-        if (val) teamBName = val
-    }
-    save()
-    renderHome()
+  if (team === "A") {
+    const val = document.getElementById("teamAInput").value;
+    if (val) teamAName = val;
+  }
+  if (team === "B") {
+    const val = document.getElementById("teamBInput").value;
+    if (val) teamBName = val;
+  }
+  save();
+  renderHome();
 }
 
-
 function renderHome() {
-    document.getElementById("teamAName").textContent = teamAName
-    document.getElementById("teamBName").textContent = teamBName
-    const listA = document.getElementById("teamAList")
-    const listB = document.getElementById("teamBList")
-    listA.innerHTML = ""
-    listB.innerHTML = ""
-    teamA.forEach(p => {
-        const li = document.createElement("li")
-        li.className = "player"
-        li.innerHTML = `
+  document.getElementById("teamAName").textContent = teamAName;
+  document.getElementById("teamBName").textContent = teamBName;
+  const listA = document.getElementById("teamAList");
+  const listB = document.getElementById("teamBList");
+  listA.innerHTML = "";
+  listB.innerHTML = "";
+  teamA.forEach((p) => {
+    const li = document.createElement("li");
+    li.className = "player";
+    li.innerHTML = `
 
 <span onclick="goToPlayer('${p.username}')">${p.username}</span>
 
@@ -49,52 +43,48 @@ function renderHome() {
 Remove
 </button>
 
-`
-        listA.appendChild(li)
-    })
-    teamB.forEach(p => {
-        const li = document.createElement("li")
-        li.className = "player"
-        li.innerHTML = `
+`;
+    listA.appendChild(li);
+  });
+  teamB.forEach((p) => {
+    const li = document.createElement("li");
+    li.className = "player";
+    li.innerHTML = `
 <span onclick="goToPlayer('${p.username}')">${p.username}</span>
 <button onclick="removePlayer('B','${p.username}')">
 Remove
 </button>
 
-`
-        listB.appendChild(li)
-    })
-
+`;
+    listB.appendChild(li);
+  });
 }
 
-
 function goToPlayer(username) {
-    localStorage.setItem("selectedPlayer", username)
-    window.location.href = "playerinfo.html"
+  localStorage.setItem("selectedPlayer", username);
+  window.location.href = "playerinfo.html";
 }
 
 function removePlayer(team, username) {
-    if (team === "A") {
-        teamA.filter(p => p.username !== username)
-    }
-    if (team === "B") {
-        teamB.filter(p => p.username !== username)
-    }
-    save()
-    renderHome()
-
+  if (team === "A") {
+    //needed to save the new mutated array
+    teamA = teamA.filter((p) => p.username !== username);
+  }
+  if (team === "B") {
+    teamB = teamB.filter((p) => p.username !== username);
+  }
+  save();
+  renderHome();
 }
 
 function usernameExists(username) {
-    return teamA.includes(username) || teamB.includes(username)
+  return teamA.includes(username) || teamB.includes(username);
 }
 
-
 function renderAddPlayer() {
+  const teamSelect = document.getElementById("teamSelect");
 
-    const teamSelect = document.getElementById("teamSelect")
-
-    teamSelect.innerHTML = `
+  teamSelect.innerHTML = `
 
 <option value="A" ${teamA.length >= 5 ? "disabled" : ""}>
 ${teamAName}
@@ -104,47 +94,42 @@ ${teamAName}
 ${teamBName}
 </option>
 
-`
+`;
 
-    document.getElementById("playerForm").addEventListener("submit", e => {
-
-        e.preventDefault()
-        const username = document.getElementById("username").value
-        if (usernameExists) {
-            document.getElementById("error").textContent = "Username already exists"
-        }
-        const player = {
-            username,
-            firstname: document.getElementById("firstname").value,
-            lastname: document.getElementById("lastname").value,
-            age: document.getElementById("age"),
-            country: document.getElementById("country").value,
-            ranking: document.getElementById("ranking")
-
-        }
-        const team = document.getElementById("teamSelect").value
-        if (team === "A") {
-            teamA.push(player)
-        }
-        if (team === "B") {
-            teamB.push(player)
-        }
-        save()
-        window.location.href = "index.html"
-
-    })
-
+  document.getElementById("playerForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    if (usernameExists) {
+      document.getElementById("error").textContent = "Username already exists";
+    }
+    const player = {
+      username,
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      age: document.getElementById("age"),
+      country: document.getElementById("country").value,
+      ranking: document.getElementById("ranking"),
+    };
+    const team = document.getElementById("teamSelect").value;
+    if (team === "A") {
+      teamA.push(player);
+    }
+    if (team === "B") {
+      teamB.push(player);
+    }
+    save();
+    window.location.href = "index.html";
+  });
 }
 
 function renderPlayerInfo() {
+  const username = localStorage.getItem("selectedPlayer");
 
-    const username = localStorage.getItem("selectedPlayer")
+  const player = teamA.find((p) => p.username === username);
 
-    const player = teamA.find(p => p.username === username)
+  const profile = document.getElementById("profile");
 
-    const profile = document.getElementById("profile")
-
-    profile.innerHTML = `
+  profile.innerHTML = `
 <div class="profile">
 <h2>${player?.username}</h2>
 <p><b>Name:</b> ${player?.firstname} ${player?.lastname}</p>
@@ -158,6 +143,5 @@ Back
 
 </div>
 
-`
-
+`;
 }
